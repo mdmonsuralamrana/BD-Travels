29ac16c3e23a6e21bf080a1b6d8c8be6c76b047b
mdmonsuralamrana <monsuralam7262@gmail.com>
2021-03-20 06:33:16 +0600

    added some features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import { Route, Router, Switch } from 'react-router';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({})
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import style from '../Header.css';
+import style from './Home.css';
+import bike from '../Photos/Frame.png';
+import car from '../Photos/Frame-1.png';
+import bus from '../Photos/Frame-2.png';
+import train from '../Photos/Group.png';
 
 const Home = () => {
     return (
-        <div className='home-page'>
-            <h1>This is Home .</h1>
+        <div className='travel-system'>
+            <div className="travel-via">
+                <img src={bike} alt=""/>
+                <p>Bike</p>
+            </div>
+            <div className="travel-via">
+                <img src={car} alt=""/>
+                <p>Car</p>
+            </div>
+            <div className="travel-via">
+                <img src={bus} alt=""/>
+                <p>Bus</p>
+            </div>
+            <div className="travel-via">
+                <img src={train} alt=""/>
+                <p>Train</p>
+            </div>
         </div>
     );
 };
