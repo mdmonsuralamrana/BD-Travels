84c9a8298e5f4b3a2020c0838029c60cccfc2cd3
mdmonsuralamrana <monsuralam7262@gmail.com>
2021-03-22 23:26:38 +0600

    assignment 9 done

--- src/Components/TravelViaDetails/TravelViaDetails.js
+++ src/Components/TravelViaDetails/TravelViaDetails.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './TravelViaDetails.css';
+import peopleIcon from '../Photos/peopleicon.png';
 
 const TravelViaDetails = (props) => {
 
-    const { travel_via, sit, price, image } = props.travelBy;
+    const {sit, price, image } = props.travelBy;
     return (
         <div className="details-box" >
             <div className="details">
                 <img src={image} alt="" />
-                <h6>{travel_via}</h6>
-                <h6>sit:{sit}</h6>
+                <h6><img src={peopleIcon} alt=""/>{sit}</h6>
                 <h6>{price}</h6>
             </div>
         </div>
